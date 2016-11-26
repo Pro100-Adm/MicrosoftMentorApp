9b389b5c088215760016fc374e46c594d20148ac
Pro100-Adm <kirow.alesha@yandex.ru>
2016-11-26 23:16:45 +0300

    Add files via upload

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 import xml.etree.ElementTree as ET
-def ip_check(ip,filename):
-    tree = ET.parse(str(filename))
+def ip_check_Azure(ip):
+    tree = ET.parse('ip_ranges_Azure.xml')
     root = tree.getroot()
     ip_correct = 0
     for i in range (0,len(ip.split('.'))):
@@ -12,18 +12,45 @@ def ip_check(ip,filename):
             x.append(ip.split('.')[i])
         for child in root:
             for child in child:
-                range_az= child.attrib['Subnet'].split('.')
+                range_azure= child.attrib['Subnet'].split('.')
                 match = 0
-                for j in range(len(range_az)):
-                    if x[j]==range_az[j]:
+                for j in range(len(range_azure)):
+                    if x[j]==range_azure[j]:
                         match+=1
                 if match >= 3:
                     print "Hoster is Microsoft Azure."
     else:
         print "Incorrect data! Please, type again."
-        
-ip = raw_input('Enter IP to find a host: ')
-ip_check(ip,'ip_ranges.xml')
+
+def ip_check_Amazon(ip):
+    tree = ET.parse('ip_ranges_Amazon.xml')
+    root = tree.getroot()
+    ip_correct = 0
+    for i in range (0,len(ip.split('.'))):
+        if len(ip.split('.'))==4 and ip.split('.')[i].isdigit():
+            ip_correct += 1
+    if ip_correct==4:
+        x = []
+        for i in range (len(ip.split('.'))):
+            x.append(ip.split('.')[i])
+        for child in root:
+            for child in child:
+                for child in child:
+                    if child.tag == 'ip_prefix':
+                        range_amazon= child.text.split('.')
+                        match = 0
+                        for j in range(len(range_amazon)):
+                            if x[j]==range_amazon[j]:
+                                match+=1
+                        if match >= 3:
+                            print "Hoster is Amazon Web Services."
+    else:
+        print "Incorrect data! Please, type again."
+    
+while True:       
+    ip = raw_input('Enter IP to find a host: ')
+    ip_check_Azure(ip)
+    ip_check_Amazon(ip)
 
 
                     
