141f5e10797cc2aa5c93b317a012a1cc970e0189
Pro100-Adm <kirow.alesha@yandex.ru>
2016-12-02 10:16:42 +0300

    Add files via upload

--- app.py
+++ app.py
@@ -2,8 +2,8 @@ def wsgi_app(environ, start_response):
     status = '200 OK'
     response_headers = [('Content-type', 'text/plain')]
     start_response(status, response_headers)
-    response_body = '<button></button>'
-    yield response_body.encode()
+    return ['<button></button>']
+    #yield response_body.encode()
 
 if __name__ == '__main__':
     from wsgiref.simple_server import make_server
