3fb3e5ee142f9a607161aef5d0887c7c9f503a69
Pro100-Adm <kirow.alesha@yandex.ru>
2016-12-02 10:20:11 +0300

    Add files via upload

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ def wsgi_app(environ, start_response):
     status = '200 OK'
     response_headers = [('Content-type', 'text/plain')]
     start_response(status, response_headers)
-    return yield ['<button></button>'].encode()
+    return '<button></button>'
     #yield response_body.encode()
 
 if __name__ == '__main__':
